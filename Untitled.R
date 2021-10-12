9df74d18fd9e3cac78de2935768a3070c056fcb8
Medelinkant <91283457+Medelinkant@users.noreply.github.com>
2021-10-12 10:38:44 -0700

    Update Untitled.R

--- Untitled.R
+++ Untitled.R
@@ -1,1 +1,22 @@
 # Coding Demo: IB516 Analytical Workflows
+#
+#additional info about the code useful to 
+# collaborators or future self
+#
+# in this case, generate data and create  a figure
+
+
+# Clear workspace and close all graphics devices --------------------------
+
+rm(list = ls())
+graphics.off()
+
+
+
+# Generate some data ------------------------------------------------------
+
+x <- rnorm(n = 100, mean = 0, sd = 1)  #independent variable
+z <- rnorm(n = 100, mean = 0, sd = 1)  #noise
+y <- 2.2 * x + 1.1 + z
+
+plot(x,y)
